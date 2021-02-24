59d6fb70e0b98139adfc3a3613ab00a6ac30b87a
ChoiMinJun <zkxm008@naver.com>
2021-02-24 20:17:29 +0900

    Blade Component

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $fruits = array('Mango','Orange','Banana','Apple','Pineapple');
+        return view('welcome',compact('fruits'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -15,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index'])->name('product.index');
 
 Route::get('/home/{name?}',[HomeController::class, 'index'])->name('home.index');
 
