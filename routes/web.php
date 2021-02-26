44109777b04065d0c046eadfa3fd2be5ecf185f9
ChoiMinJun <zkxm008@naver.com>
2021-02-26 16:43:18 +0900

    Fluent String

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\FluentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,3 +33,5 @@ Route::get('/add-post',[ClientController::class,'addPost'])->name('posts.addpost
 Route::get('/update-post', [ClientController::class, 'updatePost'])->name('posts.updatepost');
 
 Route::get('/delete-post/{id}', [ClientController::class, 'deletePost'])->name('posts.delete');
+
+Route::get('/fluent-string', [FluentController::class,'index'])->name('fluents.index');
